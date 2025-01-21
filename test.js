87a36678e5259555d058970664a8045ff736b645
Jagadeeswar_J <115331104+Jagadeeswar072@users.noreply.github.com>
2025-01-21 23:03:04 +0530

    refactor

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const getAttendance = require("./getAttendance");
 (async () => {
   try {
     const attendance = await getAttendance("23071A67F4");
-    console.log("Final Attendance Value:", attendance);
+    console.log("Attendance:", attendance);
   } catch (error) {
     console.error("Error fetching attendance:", error);
   }
